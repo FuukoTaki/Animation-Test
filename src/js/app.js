1ae05346a737a17db8183e644a19dcc8974330d3
Fuuko Taki <157997783+FuukoTaki@users.noreply.github.com>
2025-01-03 12:24:35 -0400

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,28 @@ import { Sprite } from "./sprite.js";
 let canvas;
 let ctx;
 
+// Game loop variables.
+let fps = 60;
+let interval = 1000 / fps;
+let lastTime = 0;
+let scale = 1;
+
 let sprite;
+let direction = "right";
+let animationDirection = "Front";
+let wPressed = false;
+let sPressed = false;
+let aPressed = false;
+let dPressed = false;
+let isRolling = false;
+let isAttacking = false;
 
 window.addEventListener("DOMContentLoaded", () => {
 
     // Get HTML access.
     canvas = document.getElementById("game-canvas");
     ctx = canvas.getContext("2d");
-
+    ctx.imageRendering = "crisp-edges";
     // Set canvas initial size.
     canvas.width = 400;
     canvas.height = 400;
@@ -67,8 +81,8 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     sprite = new Sprite(
-        princessAnimationsSrc, // Image source.
-        200 - 128, 200 - 128, // X and Y.
+        princessAnimationsSrc, // Object with all images source.
+        200 - 128, 200 - 128, // X and Y coordinates on canvas.
         64, 64 // Width and Height.
     );
 
@@ -82,176 +96,213 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     // Start game loop.
-    requestAnimationFrame(gameLoop);
-
-    let idleBtn = document.getElementById("idleBtn");
-    let walkBtn = document.getElementById("walkBtn");
-    let approveBtn = document.getElementById("approveBtn");
-    let declineBtn = document.getElementById("declineBtn");
-    let blinkBtn = document.getElementById("blinkBtn");
-    let cryBtn = document.getElementById("cryBtn");
-    let downBtn = document.getElementById("downBtn");
-    let hitBtn = document.getElementById("hitBtn");
-    let rollBtn = document.getElementById("rollBtn");
-    let slashBtn = document.getElementById("slashBtn");
-    let slash2Btn = document.getElementById("slash2Btn");
-    let throwBtn = document.getElementById("throwBtn");
-
-    let idle = 0;
-    let walk = 0;
-    let approve = 0;
-    let decline = 0;
-    let blink = 0;
-    let cry = 0;
-    let down = 0;
-    let hit = 0;
-    let roll = 0;
-    let slash = 0;
-    let slash2 = 0;
-    let tthrow = 0;
-
-    idleBtn.addEventListener("click", (e) => {
-
-        if (idle === 0) sprite.switchAnimation("idleFront");
-        if (idle === 1) sprite.switchAnimation("idle34");
-        if (idle === 2) sprite.switchAnimation("idleSide");
-        if (idle === 3) sprite.switchAnimation("idle34Back");
-        if (idle === 4) sprite.switchAnimation("idleBack");
-
-        idle++;
-        if (idle >= 5) idle = 0;
-    });
-
-    walkBtn.addEventListener("click", (e) => {
-
-        if (walk === 0) sprite.switchAnimation("walkFront");
-        if (walk === 1) sprite.switchAnimation("walk34");
-        if (walk === 2) sprite.switchAnimation("walkSide");
-        if (walk === 3) sprite.switchAnimation("walk34Back");
-        if (walk === 4) sprite.switchAnimation("walkBack");
-
-        walk++;
-        if (walk >= 5) walk = 0;
-    });
-
-    approveBtn.addEventListener("click", (e) => {
-
-        if (approve === 0) sprite.switchAnimation("approveFront");
-        if (approve === 1) sprite.switchAnimation("approve34");
-        if (approve === 2) sprite.switchAnimation("approveSide");
-
-        approve++;
-        if (approve >= 3) approve = 0;
-    });
-
-    declineBtn.addEventListener("click", (e) => {
-
-        if (decline === 0) sprite.switchAnimation("declineFront");
-        if (decline === 1) sprite.switchAnimation("decline34");
-        if (decline === 2) sprite.switchAnimation("declineSide");
-
-        decline++;
-        if (decline >= 3) decline = 0;
-    });
-
-    blinkBtn.addEventListener("click", (e) => {
-
-        if (blink === 0) sprite.switchAnimation("blinkFront");
-        if (blink === 1) sprite.switchAnimation("blink34");
-        if (blink === 2) sprite.switchAnimation("blinkSide");
-
-        blink++;
-        if (blink >= 3) blink = 0;
-    });
-
-    cryBtn.addEventListener("click", (e) => {
-
-        if (cry === 0) sprite.switchAnimation("cryFront");
-        if (cry === 1) sprite.switchAnimation("cry34");
-        if (cry === 2) sprite.switchAnimation("crySide");
-
-        cry++;
-        if (cry >= 3) cry = 0;
-    });
-
-    downBtn.addEventListener("click", (e) => {
-
-        if (down === 0) sprite.switchAnimation("downFront");
-        if (down === 1) sprite.switchAnimation("down34");
-        if (down === 2) sprite.switchAnimation("downSide");
-
-        down++;
-        if (down >= 3) down = 0;
-    });
-
-    hitBtn.addEventListener("click", (e) => {
-
-        if (hit === 0) sprite.switchAnimation("hitFront");
-        if (hit === 1) sprite.switchAnimation("hit34");
-        if (hit === 2) sprite.switchAnimation("hitSide");
-        if (hit === 3) sprite.switchAnimation("hit34Back");
-        if (hit === 4) sprite.switchAnimation("hitBack");
-
-        hit++;
-        if (hit >= 5) hit = 0;
-    });
-
-    rollBtn.addEventListener("click", (e) => {
-
-        if (roll === 0) sprite.switchAnimation("rollFront");
-        if (roll === 1) sprite.switchAnimation("roll34");
-        if (roll === 2) sprite.switchAnimation("rollSide");
-        if (roll === 3) sprite.switchAnimation("roll34Back");
-        if (roll === 4) sprite.switchAnimation("rollBack");
-
-        roll++;
-        if (roll >= 5) roll = 0;
-    });
-
-    slashBtn.addEventListener("click", (e) => {
-
-        if (slash === 0) sprite.switchAnimation("slashFront");
-        if (slash === 1) sprite.switchAnimation("slash34");
-        if (slash === 2) sprite.switchAnimation("slashSide");
-        if (slash === 3) sprite.switchAnimation("slash34Back");
-        if (slash === 4) sprite.switchAnimation("slashBack");
-
-        slash++;
-        if (slash >= 5) slash = 0;
-    });
-
-    slash2Btn.addEventListener("click", (e) => {
-
-        if (slash2 === 0) sprite.switchAnimation("slash2Front");
-        if (slash2 === 1) sprite.switchAnimation("slash234");
-        if (slash2 === 2) sprite.switchAnimation("slash2Side");
-        if (slash2 === 3) sprite.switchAnimation("slash234Back");
-        if (slash2 === 4) sprite.switchAnimation("slash2Back");
-
-        slash2++;
-        if (slash2 >= 5) slash2 = 0;
-    });
-
-    throwBtn.addEventListener("click", (e) => {
-
-        if (tthrow === 0) sprite.switchAnimation("throwFront");
-        if (tthrow === 1) sprite.switchAnimation("throw34");
-        if (tthrow === 2) sprite.switchAnimation("throwSide");
-        if (tthrow === 3) sprite.switchAnimation("throw34Back");
-        if (tthrow === 4) sprite.switchAnimation("throwBack");
-
-        tthrow++;
-        if (tthrow >= 5) tthrow = 0;
-    });
+    requestAnimationFrame(gameLoopController);
+    enablePlayerControllers();
 });
+
+function enablePlayerControllers() {
+
+    window.addEventListener("keydown", (e) => {
+
+        if (e.key === "w") wPressed = true;
+        if (e.key === "s") sPressed = true;
+        if (e.key === "a") aPressed = true;
+        if (e.key === "d") dPressed = true;
+        if (e.key === " ") isRolling = true;
+        if (e.key === "Enter") isAttacking = true;
+    });
+
+    window.addEventListener("keyup", (e) => {
+
+        if (e.key === "w") wPressed = false;
+        if (e.key === "s") sPressed = false;
+        if (e.key === "a") aPressed = false;
+        if (e.key === "d") dPressed = false;
+    });
+}
+
+// This function is used to control the frame rate.
+function gameLoopController(time) {
+
+    // Check ellapsed time since last frame.
+    const deltaTime = time - lastTime;
+
+    if (deltaTime > interval) {
+
+        lastTime = time - (deltaTime % interval); // Reset time.
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.scale(scale, scale);
+        gameLoop(); // Call the real game loop.
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
+
+    requestAnimationFrame(gameLoopController);
+}
 
 function gameLoop() {
 
-    // Clear canvas.
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    playerController();
+}
 
-    sprite.drawFrame(ctx);
+function playerController() {
 
-    // Call next frame.
-    requestAnimationFrame(gameLoop);
+    if (isAttacking) {
+
+        // Set the right animation.
+        if (sprite.currentAnimation.name !== ("slash" + animationDirection)) {
+            setPlayerDirection(); // Check player direction.
+            sprite.switchAnimation("slash" + animationDirection);
+        }
+
+        // Check if animation is finished.
+        if (sprite.currentAnimation.animationFinished) {
+            sprite.switchAnimation("idle" + animationDirection);
+            isAttacking = false;
+        }
+
+        sprite.drawFrame(ctx, direction);
+
+        return;
+    }
+
+    if (isRolling) {
+
+        let speed = Math.sqrt(6 * 6 + 6 * 6) / 2;
+
+        if (animationDirection === "Back") sprite.y -= 6;
+        if (animationDirection === "Front") sprite.y += 6;
+        if (animationDirection === "Side" && direction === "right") sprite.x += 6;
+        if (animationDirection === "Side" && direction === "left") sprite.x -= 6;
+        if (animationDirection === "34" && direction === "right") {
+            sprite.y += speed;
+            sprite.x += speed;
+        }
+        if (animationDirection === "34" && direction === "left") {
+            sprite.y += speed;
+            sprite.x -= speed;
+        }
+        if (animationDirection === "34Back" && direction === "right") {
+            sprite.y -= speed;
+            sprite.x += speed;
+        }
+        if (animationDirection === "34Back" && direction === "left") {
+            sprite.y -= speed;
+            sprite.x -= speed;
+        }
+
+        if (sprite.currentAnimation.name !== ("roll" + animationDirection)) {
+            setPlayerDirection();
+            sprite.switchAnimation("roll" + animationDirection);
+        }
+
+        if (sprite.currentAnimation.animationFinished) {
+            sprite.switchAnimation("idle" + animationDirection);
+            isRolling = false;
+        }
+
+        sprite.drawFrame(ctx, direction);
+
+        return;
+    }
+
+    setPlayerDirection();
+
+    // Player movement.
+    let speed = Math.sqrt(3 * 3 + 3 * 3) / 2;
+    if (wPressed && aPressed) {
+        sprite.y -= speed;
+        sprite.x -= speed;
+    } else if (wPressed && dPressed) {
+        sprite.y -= speed;
+        sprite.x += speed;
+    } else if (sPressed && aPressed) {
+        sprite.y += speed;
+        sprite.x -= speed;
+    } else if (sPressed && dPressed) {
+        sprite.y += speed;
+        sprite.x += speed;
+    } else if (wPressed) {
+        sprite.y -= 3;
+    } else if (sPressed) {
+        sprite.y += 3;
+    } else if (aPressed) {
+        sprite.x -= 3;
+    } else if (dPressed) {
+        sprite.x += 3;
+    }
+
+    // A
+    if (aPressed && !wPressed && !sPressed && !dPressed) {
+        if (sprite.currentAnimation.name !== "walkSide")
+            sprite.switchAnimation("walkSide");
+    }
+
+    // D
+    if (dPressed && !wPressed && !sPressed && !aPressed) {
+        if (sprite.currentAnimation.name !== "walkSide")
+            sprite.switchAnimation("walkSide");
+    }
+
+    // W an A
+    if (wPressed && aPressed) {
+        if (sprite.currentAnimation.name !== "walk34Back")
+            sprite.switchAnimation("walk34Back");
+
+        // W and D
+    } else if (wPressed && dPressed) {
+        if (sprite.currentAnimation.name !== "walk34Back")
+            sprite.switchAnimation("walk34Back");
+
+        // W
+    } else if (wPressed) {
+        if (sprite.currentAnimation.name !== "walkBack")
+            sprite.switchAnimation("walkBack");
+    }
+
+    // S and A
+    if (sPressed && aPressed) {
+        if (sprite.currentAnimation.name !== "walk34")
+            sprite.switchAnimation("walk34");
+
+        // S and D
+    } else if (sPressed && dPressed) {
+        if (sprite.currentAnimation.name !== "walk34")
+            sprite.switchAnimation("walk34");
+
+        // S
+    } else if (sPressed) {
+        if (sprite.currentAnimation.name !== "walkFront")
+            sprite.switchAnimation("walkFront");
+    }
+
+    // Idle animation.
+    if (!wPressed && !sPressed && !aPressed && !dPressed) {
+
+        if (sprite.currentAnimation.name !== ("idle" + animationDirection)) {
+            sprite.switchAnimation("idle" + animationDirection);
+        }
+
+        if (sprite.currentAnimation.animationFinished) {
+            sprite.switchAnimation("roll" + animationDirection);
+        }
+    }
+
+    sprite.drawFrame(ctx, direction);
+}
+
+function setPlayerDirection() {
+
+    // Player direction.
+    if (aPressed) direction = "left";
+    if (dPressed) direction = "right";
+
+    // Player animation direction.
+    if (wPressed) animationDirection = "Back";
+    if (sPressed) animationDirection = "Front";
+    if (aPressed) animationDirection = "Side";
+    if (dPressed) animationDirection = "Side";
+    if (wPressed && aPressed) animationDirection = "34Back";
+    if (wPressed && dPressed) animationDirection = "34Back";
+    if (sPressed && aPressed) animationDirection = "34";
+    if (sPressed && dPressed) animationDirection = "34";
 }
--- src/js/sprite.js
+++ src/js/sprite.js
@@ -3,7 +3,7 @@ export class Sprite {
     constructor(animationsSrc, x, y, width, height) {
 
         // Set initial values.
-        this.animationsSrc = animationsSrc;
+        this.animationsSrc = animationsSrc; // Object with all animations.
         this.x = x;
         this.y = y;
         this.width = width;
@@ -25,25 +25,25 @@ export class Sprite {
             walkBack: new Animation(this.animationsSrc["walk"], "walkBack", 256, 6, 10),
 
             // -- SLASH --
-            slashFront: new Animation(animationsSrc["slash"], "slashFront", 0, 6, 10),
-            slash34: new Animation(animationsSrc["slash"], "slash34", 64, 6, 10),
-            slashSide: new Animation(animationsSrc["slash"], "slashSide", 128, 6, 10),
-            slash34Back: new Animation(animationsSrc["slash"], "slash34Back", 192, 6, 10),
-            slashBack: new Animation(animationsSrc["slash"], "slashBack", 256, 6, 10),
+            slashFront: new Animation(animationsSrc["slash"], "slashFront", 0, 6, 6, false),
+            slash34: new Animation(animationsSrc["slash"], "slash34", 64, 6, 6, false),
+            slashSide: new Animation(animationsSrc["slash"], "slashSide", 128, 6, 6, false),
+            slash34Back: new Animation(animationsSrc["slash"], "slash34Back", 192, 6, 6, false),
+            slashBack: new Animation(animationsSrc["slash"], "slashBack", 256, 6, 6, false),
 
             // -- SLASH 2 --
-            slash2Front: new Animation(animationsSrc["slash2"], "slash2Front", 0, 6, 10),
-            slash234: new Animation(animationsSrc["slash2"], "slash234", 64, 6, 10),
-            slash2Side: new Animation(animationsSrc["slash2"], "slash2Side", 128, 6, 10),
-            slash234Back: new Animation(animationsSrc["slash2"], "slash234Back", 192, 6, 10),
-            slash2Back: new Animation(animationsSrc["slash2"], "slash2Back", 256, 6, 10),
+            slash2Front: new Animation(animationsSrc["slash2"], "slash2Front", 0, 6, 10, false),
+            slash234: new Animation(animationsSrc["slash2"], "slash234", 64, 6, 10, false),
+            slash2Side: new Animation(animationsSrc["slash2"], "slash2Side", 128, 6, 10, false),
+            slash234Back: new Animation(animationsSrc["slash2"], "slash234Back", 192, 6, 10, false),
+            slash2Back: new Animation(animationsSrc["slash2"], "slash2Back", 256, 6, 10, false),
 
             // -- ROLL --
-            rollFront: new Animation(animationsSrc["roll"], "rollFront", 0, 11, 10),
-            roll34: new Animation(animationsSrc["roll"], "roll34", 64, 11, 10),
-            rollSide: new Animation(animationsSrc["roll"], "rollSide", 128, 11, 10),
-            roll34Back: new Animation(animationsSrc["roll"], "roll34Back", 192, 11, 10),
-            rollBack: new Animation(animationsSrc["roll"], "rollBack", 256, 11, 10),
+            rollFront: new Animation(animationsSrc["roll"], "rollFront", 0, 11, 4, false),
+            roll34: new Animation(animationsSrc["roll"], "roll34", 64, 11, 4, false),
+            rollSide: new Animation(animationsSrc["roll"], "rollSide", 128, 11, 4, false),
+            roll34Back: new Animation(animationsSrc["roll"], "roll34Back", 192, 11, 4, false),
+            rollBack: new Animation(animationsSrc["roll"], "rollBack", 256, 11, 4, false),
 
             // -- CRY --
             cryFront: new Animation(animationsSrc["cry"], "cryFront", 0, 5, 10),
@@ -71,57 +71,90 @@ export class Sprite {
             downSide: new Animation(animationsSrc["down"], "downSide", 128, 4, 10),
 
             // -- HIT --
-            hitFront: new Animation(animationsSrc["hit"], "hitFront", 0, 4, 10),
-            hit34: new Animation(animationsSrc["hit"], "hit34", 64, 4, 10),
-            hitSide: new Animation(animationsSrc["hit"], "hitSide", 128, 4, 10),
-            hit34Back: new Animation(animationsSrc["hit"], "hit34Back", 192, 4, 10),
-            hitBack: new Animation(animationsSrc["hit"], "hitBack", 256, 4, 10),
+            hitFront: new Animation(animationsSrc["hit"], "hitFront", 0, 4, 10, false),
+            hit34: new Animation(animationsSrc["hit"], "hit34", 64, 4, 10, false),
+            hitSide: new Animation(animationsSrc["hit"], "hitSide", 128, 4, 10, false),
+            hit34Back: new Animation(animationsSrc["hit"], "hit34Back", 192, 4, 10, false),
+            hitBack: new Animation(animationsSrc["hit"], "hitBack", 256, 4, 10, false),
 
             // -- THROW --
-            throwFront: new Animation(animationsSrc["throw"], "throwFront", 0, 12, 10),
-            throw34: new Animation(animationsSrc["throw"], "throw34", 64, 12, 10),
-            throwSide: new Animation(animationsSrc["throw"], "throwSide", 128, 12, 10),
-            throw34Back: new Animation(animationsSrc["throw"], "throw34Back", 192, 12, 10),
-            throwBack: new Animation(animationsSrc["throw"], "throwBack", 256, 12, 10),
+            throwFront: new Animation(animationsSrc["throw"], "throwFront", 0, 12, 10, false),
+            throw34: new Animation(animationsSrc["throw"], "throw34", 64, 12, 10, false),
+            throwSide: new Animation(animationsSrc["throw"], "throwSide", 128, 12, 10, false),
+            throw34Back: new Animation(animationsSrc["throw"], "throw34Back", 192, 12, 10, false),
+            throwBack: new Animation(animationsSrc["throw"], "throwBack", 256, 12, 10, false),
         }
 
         // Set initial animation.
-        this.currentAnimation = this.animations.walkFront;
+        this.currentAnimation = this.animations.idleFront;
     }
 
     switchAnimation(name) {
         this.currentAnimation.animationFrameCurrent = 0;
         this.currentAnimation.delayBetweenFramesCurrent = 0;
+        this.currentAnimation.animationFinished = false;
         this.currentAnimation = this.animations[name];
     }
 
-    drawFrame(ctx) {
+    drawFrame(ctx, direction) {
 
-        ctx.drawImage(
-            this.currentAnimation.animationSrc, // Image source.
-            0 + this.currentAnimation.animationFrameCurrent * this.width, this.currentAnimation.position, // X and Y coordinates on src.
-            this.width, this.height, // Width and Height on src.
-            this.x, this.y, // X and Y on canvas.
-            this.width * 4, this.height * 4 // Width and height on canvas.
-        )
+        if (direction === "right") {
 
+            ctx.drawImage(
+                this.currentAnimation.animationSrc, // Image source.
+                0 + this.currentAnimation.animationFrameCurrent * this.width, this.currentAnimation.position, // X and Y coordinates on src.
+                this.width, this.height, // Width and Height on src.
+                this.x, this.y, // X and Y on canvas.
+                this.width * 4, this.height * 4 // Width and height on canvas.
+            )
+        }
+
+        if (direction === "left") {
+
+            ctx.save();
+            ctx.scale(-1, 1);
+
+            ctx.drawImage(
+                this.currentAnimation.animationSrc, // Image source.
+                0 + this.currentAnimation.animationFrameCurrent * this.width, this.currentAnimation.position, // X and Y coordinates on src.
+                this.width, this.height, // Width and Height on src.
+                -(this.x + this.width * 4), this.y, // X and Y on canvas.
+                this.width * 4, this.height * 4 // Width and height on canvas.
+            )
+
+            ctx.restore();
+        }
+
+        // Delay.
         this.currentAnimation.delayBetweenFramesCurrent--;
         if (this.currentAnimation.delayBetweenFramesCurrent <= 0) {
             this.currentAnimation.delayBetweenFramesCurrent = this.currentAnimation.delayBetweenFrames;
+
+            // Current frame.
             this.currentAnimation.animationFrameCurrent++;
-            if (this.currentAnimation.animationFrameCurrent >= this.currentAnimation.animationFrames)
-                this.currentAnimation.animationFrameCurrent = 0;
+            if (this.currentAnimation.animationFrameCurrent >= this.currentAnimation.animationFrames) {
+                // Check if the animation is on loop mode.
+                if (this.currentAnimation.loop) {
+                    this.currentAnimation.animationFrameCurrent = 0;
+                } else {
+                    this.currentAnimation.animationFrameCurrent = this.currentAnimation.animationFrames - 1;
+                    this.currentAnimation.animationFinished = true;
+                }
+            }
         }
     }
 }
 
 class Animation {
 
-    constructor(animationSrc, name, position, animationFrames, delayBetweenFrames) {
+    constructor(animationSrc, name, position, animationFrames, delayBetweenFrames,
+        loop = true) {
 
         this.animationSrc = animationSrc;
         this.name = name;
         this.position = position;
+        this.loop = loop;
+        this.animationFinished = false;
 
         this.animationFrameCurrent = 0;
         this.animationFrames = animationFrames;
